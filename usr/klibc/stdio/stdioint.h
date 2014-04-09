e845d9b10c5699ef5f2f41a117f2211508f3d6bf
H. Peter Anvin <hpa@linux.intel.com>
2014-04-09 16:41:47 -0700

    [klibc] Remove obsolete enum _IO_file_flags
    This enum is not used anywhere.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- usr/klibc/stdio/stdioint.h
+++ usr/klibc/stdio/stdioint.h
@@ -28,15 +28,6 @@ struct _IO_file_pvt {
 
 #define stdio_pvt(x) container_of(x, struct _IO_file_pvt, pub)
 
-enum _IO_file_flags {
-	_IO_FILE_FLAG_WRITE	=  1, /* Buffer has write data */
-	_IO_FILE_FLAG_READ	=  2, /* Buffer has read data */
-	_IO_FILE_FLAG_LINE_BUF  =  4, /* Line buffered */
-	_IO_FILE_FLAG_UNBUF     =  8, /* Unbuffered */
-	_IO_FILE_FLAG_EOF	= 16,
-	_IO_FILE_FLAG_ERR	= 32,
-};
-
 /* Assign this much extra to the input buffer in case of ungetc() */
 #define _IO_UNGET_SLOP	32
 
