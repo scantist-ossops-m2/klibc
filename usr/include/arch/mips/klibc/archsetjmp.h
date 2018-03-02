edf92a18d1f1725896c928cbcf580abc268f307c
James Cowgill <james.cowgill@mips.com>
2018-03-02 08:33:03 -0800

    [klibc] mips: don't save floating point registers in setjmp / longjmp
    Don't bother saving any floating point registers in setjmp or restoring
    them in longjmp. This causes issues when compiling for the FPXX ABI, and
    is unnessesary as klibc does not support floating point code.
    Additionally, remove the unused parts of __jmp_buf.
    
    Signed-off-by: James Cowgill <james.cowgill@mips.com>
    Link: https://www.zytor.com/pipermail/klibc/2018-March/003983.html
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- usr/include/arch/mips/klibc/archsetjmp.h
+++ usr/include/arch/mips/klibc/archsetjmp.h
@@ -18,20 +18,6 @@ struct __jmp_buf {
 	unsigned long __sp;
 	unsigned long __s8;
 	unsigned long __ra;
-	unsigned long __f20;
-	unsigned long __f21;
-	unsigned long __f22;
-	unsigned long __f23;
-	unsigned long __f24;
-	unsigned long __f25;
-	unsigned long __f26;
-	unsigned long __f27;
-	unsigned long __f28;
-	unsigned long __f29;
-	unsigned long __f30;
-	unsigned long __f31;
-	unsigned long __fcr31;
-	unsigned long __unused;
 } __attribute__ ((aligned(8)));
 
 typedef struct __jmp_buf jmp_buf[1];
